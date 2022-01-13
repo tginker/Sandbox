cfb5d9e5d3ec99002a0f502a7a9675c161c43bc9
Tanya Ginker <tginker@gmail.com>
2022-01-13 18:43:52 +0200

    some solutions

---
+++ switch-statements.js
@@ -0,0 +1,105 @@
+//1
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val){
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
+//2
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val) {
+    case "bob": 
+      answer = "Marley";
+      break;
+    case 42: 
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+  
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+
+//3
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val){
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
+//4
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
